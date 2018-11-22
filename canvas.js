6bd865c3b16adc3bef7286246b98bd60130978b3
Andreas Christian <andreaschristian40@gmail.com>
2018-11-22 14:55:41 +0700

    Canvas Circle and Random Color

--- canvas.js
+++ canvas.js
@@ -4,5 +4,37 @@ canvas.width = window.innerWidth; // set width canvas element
 canvas.height = window.innerHeight; // set height canvas element
 
 let c = canvas.getContext('2d'); // insert object to canvas
-c.fillRect(100,100,100,100); // object 1st 
-c.fillRect(200,200,200,200); // object 2nd
+c.fillStyle = 'rgba(255,0,0,0.5)'; // set color
+c.fillRect(450,200,100,100); // object 1st //x , y, width ,height
+c.fillStyle = 'rgba(0,255,0,0.5)';
+c.fillRect(250,200,100,100); // object 2nd
+
+
+// line
+c.beginPath();
+c.moveTo(50,300); //x y
+c.lineTo(400,20);
+c.lineTo(800,350);
+c.strokeStyle = "#fa34a3"; // set color
+c.stroke();
+
+// Arc / cricle 
+c.beginPath();
+c.arc(400, 100, 30,0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();
+
+for(let i = 0 ; i < 3 ; i++){
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.arc(x,y,30,0,Math.PI * 2, false);
+    c.strokeStyle = getRndColor(); // random color
+    c.stroke();
+}
+function getRndColor() { //color random function
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
